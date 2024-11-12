37bbda8028b7714c17e72e92ec7bf930ef18717d
Lux Reyes <136732364+Luxanna22@users.noreply.github.com>
2024-11-13 02:46:00 +0800

    Update ai.js
    update api

--- ws3/commands/ai.js
+++ ws3/commands/ai.js
@@ -1,17 +1,15 @@
 const axios = require("axios");
-const name = "ai" ;
+const name = "lux" ;
 
 module.exports = {
   name,
   description: "Interact with ChatGPT-4o",
   async run ({ api, event, send, args }){
     const prompt = args.join(" ");
-    if (!prompt) return send(`Please enter your question! 
-
-Example: ${api.prefix + name} what is love?`);
+    if (!prompt) return send(`Greetings! how can I help you?`);
     send("Please wait... 🔎");
     try {
-    const gpt = await axios.get(`${api.api_josh}/api/gpt-4o`, {
+    const gpt = await axios.get(`https://joshweb.click/api/gpt-4o`, {
       params: {
         q: prompt,
         uid: event.sender.id
@@ -19,9 +17,7 @@ Example: ${api.prefix + name} what is love?`);
     });
     if (!gpt || !gpt.data.status)
     throw new Error();
-    send(`${gpt.data.result}
-
-🤖 WieAI by Neth Aceberos`);
+    send(`${gpt.data.result}`);
     } catch(err){
       send(err.message || err);
       return;
